628fb70dfe711d3fb6a115ab52ed5ffa55df1c36
sachin <sachin.vishwakarmas390@gmail.com>
2023-02-16 16:53:03 +0530

    Java -Tree - Max Dept Of Binary Tree

---
+++ java/Tree/MaxDeptOfTree.java
@@ -0,0 +1,44 @@
+public class MaxDeptOfTree{
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { 
+            this.val = val; 
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right)+1;
+    }
+
+    public static void inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.val + " ");
+        inorderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        int maxDepth = maxDepth(root);
+        System.out.println("Maximum depth of binary tree: " + maxDepth);
+    }
+}
